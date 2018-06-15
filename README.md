a4068eac5b29d6de5361942307159b3058a50878
aklam <aklam@princeton.edu>
2018-06-15 16:03:21 +0200

    reads definitions from all word files

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 TODO:
 - get playing field to dynamically size to window 
-- center scores
 - dictionary lookup
+- refactor loading bar
--- word_defs.py
+++ word_defs.py
@@ -1,10 +1,25 @@
+import os
+import glob
 from bs4 import BeautifulSoup
 
-defs = open("wb1913_a.html",encoding='iso-8859-15')
-soup = BeautifulSoup(defs,"lxml") 
-word_defs = soup.body.find_all("p")
+def get_word_definitions():
+	path_to_defs = os.getcwd() + '/OPTED/v003/'
+	all_def_files = os.listdir(path_to_defs)[1:]
 
-for wd in word_defs:
-	print(wd.b.string)
-	print(wd.contents[3][2:])
-#print(soup.find_all("P"))
+	word_defs = {}
+
+	for def_file in all_def_files:
+		print(def_file)
+		defs = open(path_to_defs + def_file,encoding='iso-8859-15')
+		soup = BeautifulSoup(defs,"lxml") 
+		word_defs_html = soup.body.find_all("p")
+
+		for entry in word_defs_html:
+			word_lower = entry.b.string.lower()
+			if word_lower not in word_defs:
+				word_defs[word_lower] = entry.contents[3][2:]
+
+	return word_defs
+
+
+print(get_word_definitions()["ours"])
